aed20639391d9eb7db37664e7e1225deebfe18d7
Peter Hahn <peterkunho@gmail.com>
2019-04-18 17:38:59 -0400

    Finished a working version of the tensorflow model, in terms of dimensions

--- model/model.py
+++ model/model.py
@@ -8,32 +8,43 @@ class ImageCorpus:
         self.image_width = 5
         self.image_height = 5
 
-        self.learning_rate = 1e-3
 
-        self.forward_pass = self.forward_pass_tensor()
-
-        self.loss = self.loss_tensor()
-        self.optimize = self..optimize_tensor()
         
 
 class Model:
     def __init__(self, corpus):
         self.corpus = corpus
+        self.image_width = corpus.image_width 
+        self.image_height = corpus.image_height
+
+
+        self.learning_rate = 1e-3
+
+
 
         # below currently uses black and white images. We can change if need be later. 
-        self.input = tf.placeholder(tf.float32, shape=[None, corpus.image_width, corpus.image_height])
+        self.input = tf.placeholder(tf.float32, shape=[None, self.image_width, self.image_height])
 
         # These labels will be an integer that represents the park that the given image is associated with
         self.output = tf.placeholder(tf.int32, shape=[None])
 
+
+        self.forward_pass = self.forward_pass_tensor()
+        print(self.forward_pass.shape)
+
+        self.loss = self.loss_tensor()
+        self.optimize = self.optimize_tensor()
+
     def forward_pass_tensor(self):
-        pass
+        flattened = tf.reshape(self.input, [-1, self.image_width * self.image_height])
+        return tf.layers.dense(flattened, 1)
+        
 
     def loss_tensor(self):
-        pass
+        return tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.input, logits=self.forward_pass)
 
     def optimize_tensor(self):
-        return tf.train.AdamOptimizer(learning_rate=self.learning_rate).optimize(self.loss)
+        return tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.loss)
 
     def train(self):
         pass
@@ -41,3 +52,13 @@ class Model:
 
     def test(self):
         pass
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    print('Hi, if you are seeing this, welcome to debugging the model!')
+    model = Model(ImageCorpus())
