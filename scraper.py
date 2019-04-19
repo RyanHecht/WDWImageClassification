1046e5db058a41c1f9ab24648ce73f3f912e8e9f
Ryan Hecht <ryanhecht.tse@gmail.com>
2019-04-19 13:28:14 -0400

    Got scraping to work properly

--- scraper.py
+++ scraper.py
@@ -16,19 +16,24 @@ with open(csv_path, "r") as f:
     for line in f.readlines():
         coords.append([float(line.split(",")[i]) for i in range(len(line.split(",")))])
 coords = np.array(coords)
-max_lat = round(max(coords[:, 0]), 4)
-max_long = round(max(coords[:, 1]), 4)
-min_lat = round(min(coords[:, 0]), 4)
-min_long = round(min(coords[:, 1]), 4)
+# max_lat = np.around(max(coords[:,0]), decimals=3)
+# max_long = np.around(max(coords[:, 1]), decimals=3)
+# min_lat = np.around(min(coords[:, 0]), decimals=3)
+# min_long = np.around(min(coords[:, 1]), decimals=3)
+max_lat = "{:.3f}".format(max(coords[:,0]))
+max_long = "{:.3f}".format(max(coords[:, 1]))
+min_lat = "{:.3f}".format(min(coords[:, 0]))
+min_long = "{:.3f}".format(min(coords[:, 1]))
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
-bbox_coords = [max_lat, max_long, min_lat, min_long]
+bbox_coords = [min_long, min_lat, max_long, max_lat]
 print(bbox_coords)
-photos_search = flickr.photos.search(bbox = bbox_coords)
+photos_search = flickr.photos.search(bbox = ",".join(bbox_coords), min_upload_date = 1524009600)
 assert(photos_search['stat'] == 'ok')
 photos = photos_search['photos']['photo']
-pdb.set_trace()
+#pdb.set_trace()
 with open("results.json", "w") as g:
     g.write(json.dumps(photos_search))
 g.close()
 for im in photos:
-    flickr.photos.getSizes(im['id'])
+    print(im)
+    #flickr.photos.getSizes(im['id'])
