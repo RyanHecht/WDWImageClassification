bbf964b3fce1f251e53c89667ff654af71e4b4f1
Ryan Hecht <ryanhecht.tse@gmail.com>
2019-04-23 15:11:17 -0400

    work from weekend

--- common.py
+++ common.py
@@ -10,19 +10,19 @@ import urllib.request
 import csv
 
 
-# Save the Flickr image with id `image_id` of size `size` to the folder specified in `path`
-# It will be saved as `path`/`image_id`.jpg
-# Its lat/lng will be saved in `path`/labels/`image_id`.json
-def save_image(image_id, size, path):
-    sizes = flickr.photos.getSizes(photo_id=image_id)['sizes']['size']
+# Save the Flickr image with name `image_name` of size `size` to the folder specified in `path`
+# It will be saved as `path`/`image_name`.jpg
+# Its lat/lng will be saved in `path`/labels/`image_name`.json
+def save_image(image_name, size, path):
+    sizes = flickr.photos.getSizes(photo_name=image_name)['sizes']['size']
 
     medium_size = next(s for s in sizes if s['label'] == size)
-    urllib.request.urlretrieve(medium_size['source'], path + "/" + image_id + ".jpg")
+    urllib.request.urlretrieve(medium_size['source'], path + "/" + image_name + ".jpg")
 
-    location = flickr.photos.geo.getLocation(photo_id=image_id)['photo']['location']
+    location = flickr.photos.geo.getLocation(photo_name=image_name)['photo']['location']
     lat_lng = {'lat': location['latitude'], 'lng': location['longitude']}
     print(lat_lng)
-    update_label(path + "/labels/" + image_id + ".json", {'location': lat_lng})
+    update_label(path + "/labels/" + image_name + ".json", {'location': lat_lng})
 
 
 # Update the json in `file` with the values in the dictionary `dict`. File will be created if it doesn't exist,
