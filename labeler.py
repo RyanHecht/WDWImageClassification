e8b5cfa6c759de8aa698755c07bf7ecb21cba72e
Ryan Hecht <ryanhecht.tse@gmail.com>
2019-04-24 19:37:32 -0400

    added labeler stats

--- labeler.py
+++ labeler.py
@@ -32,21 +32,39 @@ def get_label(lat, lng, regions):
 
 
 
-park_regions = fetch_regions('parks')
-land_regions = fetch_regions('lands')
+if len(sys.argv) == 2:
+        if sys.argv[1] == "stats":
+                stats = {}
+                for filename in os.listdir("data/labels"):
+                        with open(filename, 'r') as file:
+                                try:
+                                        data = json.load(file)
+                                        park = data['labels']['park']
+                                        land = data['labels']['land']
+                                        stats[park] = stats.get(park, 0) + 1
+                                        stats[land] = stats.get(land, 0) + 1
+                                
+                                except Exception as e:
+                                        print(e.read())
+                        print(stats)
+else:
 
-label_dir = "data/labels"
-for label_file in os.listdir(label_dir):
-    with open(label_dir + "/" + label_file, 'r') as file:
-        print(label_file)
-        data = json.load(file)
-        location = data['location']
-        point = (float(location['lat']), float(location['lng']))
 
-        park_label = get_label(point[0], point[1], park_regions)
-        land_label = get_label(point[0], point[1], land_regions)
-        data.update({"labels": {"park": park_label, "land": land_label}})
-        print(data)
-        common.update_label(label_dir + "/" + label_file, data)
+        park_regions = common.fetch_regions('parks')
+        land_regions = common.fetch_regions('lands')
+
+        label_dir = "data/labels"
+        for label_file in os.listdir(label_dir):
+                with open(label_dir + "/" + label_file, 'r') as file:
+                        print(label_file)
+                        data = json.load(file)
+                        location = data['location']
+                        point = (float(location['lat']), float(location['lng']))
+
+                        park_label = common.get_label(point[0], point[1], park_regions)
+                        land_label = common.get_label(point[0], point[1], land_regions)
+                        data.update({"labels": {"park": park_label, "land": land_label}})
+                        print(data)
+                        common.update_label(label_dir + "/" + label_file, data)
         
     
