f530076501f297fdb8ec30923139a2e2b1770e0e
rhecht <ryanhecht.tse@gmail.com>
2019-05-05 21:24:18 -0400

    Predictor draft

--- predictor/web.py
+++ predictor/web.py
@@ -1,7 +1,17 @@
-from flask import Flask, current_app
+from flask import Flask, current_app, json, request
 from gevent.pywsgi import WSGIServer
+import numpy as np
+import scipy.ndimage
+import cv2
+import tensorflow as tf
+import argparse
+import sys
+
 app = Flask(__name__)
 
+session = ""
+input_tensor = ""
+predict_tensor = ""
 
 @app.route('/', methods = ['GET'])
 def index():
@@ -9,9 +19,43 @@ def index():
 
 @app.route('/upload', methods = ['POST'])
 def upload():
-    return {"test": "555"}
+	filestr = request.files['file'].read()
+	npimg = np.fromstring(filestr, np.uint8)
+	image = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+
+	image = ((image / 255) - 0.5) * 2
+
+
+	image = cv2.resize(image, dsize=(600, 400), interpolation=cv2.INTER_CUBIC)
+
+	images = np.array([image])
+	return json.dumps(predict(image))
+
+def predict(image):
+	images = np.array([image])
+	return session.run(predict_tensor,feed_dict={input:images})
 
 if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Run webserver that will predict the park/land of an image')
+	parser.add_argument('-model', help='Path to the model name. e.g., "park/model-epoch3-100"')
+	parser.parse_args()
+	model = sys.argv[2]
+	model_path = "../model/" + model
+	session = tf.Session()
+	print("Recreating graph structure from meta file\n")
+	"../model/"
+	new_saver = tf.train.import_meta_graph(model_path + ".meta")
+	print("Restoring variables from %s\n" % model_path)
+	new_saver.restore(session, model_path)
+	print("Initializing...")
+	session.run(tf.global_variables_initializer())
+	print("Getting default graph...")
+	graph = tf.get_default_graph()
+	print("Getting input tensor")
+	input_tensor = graph.get_tensor_by_name("Placeholder:0")
+	print("Getting prediction tensor")
+	predict_tensor = graph.get_tensor_by_name("dense_2/BiasAdd:0")
 	print("Starting webserver")
 	http_server = WSGIServer(('', 8080), app)
 	http_server.serve_forever()
