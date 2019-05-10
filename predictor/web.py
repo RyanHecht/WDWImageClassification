9ae5fcd31154f5607b69b58e970e5cb54186bd5e
rhecht <ryanhecht.tse@gmail.com>
2019-05-10 13:26:02 -0400

    Added geo model to predictor

--- predictor/web.py
+++ predictor/web.py
@@ -68,9 +68,9 @@ class SavedGeoModel:
 			print(name + ": Initializing global variables")
 			self.session.run(tf.global_variables_initializer())
 			print(name + ": Getting tensors")
-			self.input_tensor = self.graph.get_tensor_by_name("input")
-			self.predict_tensor = self.graph.get_tensor_by_name("forwardpass")
-			self.dropout_tensor = self.graph.get_tensor_by_name("training")
+			self.input_tensor = self.graph.get_tensor_by_name("input:0")
+			self.predict_tensor = self.graph.get_tensor_by_name("forwardpass:0")
+			self.dropout_tensor = self.graph.get_tensor_by_name("training:0")
 	
 	def predict(self, image):
 		images = np.array([image])
